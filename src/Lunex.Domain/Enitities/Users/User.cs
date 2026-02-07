46be08e9f907d165bd0317cb408c48a06e0e32b4
Kevin Mathew <kevinbmathew00@gmail.com>
2026-02-07 23:56:36 +0530

    added password/salt in users entity

--- src/Lunex.Domain/Enitities/Users/User.cs
+++ src/Lunex.Domain/Enitities/Users/User.cs
@@ -5,10 +5,6 @@ public sealed class User
     public string Id { get; set; } = Guid.NewGuid().ToString();
     public required string Name { get; set; }
     public required string Email { get; set; }
-    public static User Empty => new()
-    {
-        Id = string.Empty,
-        Name = string.Empty,
-        Email = string.Empty
-    };
+    public required byte[] PasswordHash { get; set; }
+    public required byte[] PasswordSalt { get; set; }
 }
--- src/Lunex.Infrastructure/src/Lunex.Infrastructure/Migrations/20260207181011_AddedPasswordFieldInUserEntity.Designer.cs
+++ src/Lunex.Infrastructure/src/Lunex.Infrastructure/Migrations/20260207181011_AddedPasswordFieldInUserEntity.Designer.cs
@@ -2,17 +2,20 @@
 using Lunex.Infrastructure.Persistance;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 #nullable disable
 
-namespace Lunex.Infrastructure.Migrations
+namespace Lunex.Infrastructure.src.Lunex.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260207181011_AddedPasswordFieldInUserEntity")]
+    partial class AddedPasswordFieldInUserEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
